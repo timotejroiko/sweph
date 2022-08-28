ba9c8fbd49fddf732fe5003003728c0fed5d6b1d
Timotej Rojko <33236065+timotejroiko@users.noreply.github.com>
2022-08-28 17:22:35 -0300

    swiss ephemeris 2.10.03

--- src/functions/set_ephe_path.cpp
+++ src/functions/set_ephe_path.cpp
@@ -9,6 +9,6 @@ void sweph_set_ephe_path(const Napi::CallbackInfo& info) {
 	if(!sweph_type_check(args, info)) {
 		return;
 	}
-	swe_set_ephe_path(info[0].IsString() ? (char*)info[0].As<Napi::String>().Utf8Value().c_str() : NULL);
+	swe_set_ephe_path(info[0].IsString() ? info[0].As<Napi::String>().Utf8Value().c_str() : NULL);
 	return;
 }
