f90564fc4c3894e9449a0cc79266f7c1226da943
Timotej Rojko <33236065+timotejroiko@users.noreply.github.com>
2026-06-28 18:08:15 -0300

    fix incorrect parameter error in lun_occult_when_loc

--- src/functions/lun_occult_when_loc.cpp
+++ src/functions/lun_occult_when_loc.cpp
@@ -16,8 +16,8 @@ Napi::Value sweph_lun_occult_when_loc(const Napi::CallbackInfo& info) {
 		return env.Null();
 	}
 	double geopos [3];
-	if(!sweph_double_array_converter(geopos, 3, info[0])) {
-		Napi::TypeError::New(env, args[1].second).ThrowAsJavaScriptException();
+	if(!sweph_double_array_converter(geopos, 3, info[4])) {
+		Napi::TypeError::New(env, args[5].second).ThrowAsJavaScriptException();
 		return env.Null();
 	}
 	double ret [10];
